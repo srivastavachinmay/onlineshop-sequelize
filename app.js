11bfd7bc3b76c17cb6a219891067a60fcc890fd6
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-08-04 17:49:44 +0530

    express-js
    import html file
    routing
    navigation

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const path = require('path');
 
-const app =express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use('/',(req,res,next)=>{
-    next() // Allows the request to continue to the next middleware in line
-})
-app.use('/add-product',(req,res,next)=>{
-    res.send(' <form action="/product" method="POST"><input type="text" name="name"><button type="submit">Add Product</button></form>')
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    res.send('Hello from Express')
-    next() // Allows the request to continue to the next middleware in line
-})
+const app = express();
 
-// const server = http.createServer(app)
-// server.listen(3000)
-app.listen(3000)
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
