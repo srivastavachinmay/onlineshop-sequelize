cec08508bebab2a3646f9a2a9054901cb33af1f4
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-08-11 03:17:08 +0530

    err

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const products = []
 module.exports = class Product {
     constructor(title) {
@@ -6,21 +9,32 @@ module.exports = class Product {
 
 
     save() {
-        products.push(this)
+        // products.push(this)
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+        fs.readFile(p, (err, filecontent) => {
+            let products = []
+            if (!err) {
+                products = JSON.parse(JSON.stringify(fileContent));
+            }
+            products.push(this)
+            console.log(filecontent)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        });
     }
 
-    static fetchAll(){
-        return products
+    static fetchAll(callback) {
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                console.log(err)
+                callback([]);
+            }
+            callback(JSON.parse(JSON.stringify(fileContent)))
+
+        })
     }
 }
 
-
-
-/*
-
-Product p=new Product('hell')
-p.save()
-
-
-
- */
