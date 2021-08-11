8fb4183c3396b960dfd160139e236fc82c64348e
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-08-11 13:18:09 +0530

    random

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.json())// app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
--- models/product.js
+++ models/product.js
@@ -1,40 +1,37 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const products = []
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-    constructor(title) {
-        this.title = title
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    save() {
-        // products.push(this)
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
-        fs.readFile(p, (err, filecontent) => {
-            let products = []
-            if (!err) {
-                products = JSON.parse(JSON.stringify(fileContent));
-            }
-            products.push(this)
-            console.log(filecontent)
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err)
-            })
-        });
-    }
-
-    static fetchAll(callback) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
-
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                console.log(err)
-                callback([]);
-            }
-            callback(JSON.parse(JSON.stringify(fileContent)))
-
-        })
-    }
-}
-
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
