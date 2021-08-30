23fb3e24abac2a62cf84b7c1fdda1339e826af06
Chinmay Srivastava <61424879+srivastavachinmay@users.noreply.github.com>
2021-08-31 04:49:07 +0530

    Associations completed for Product controller using sequelize

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
+const User = require('./models/user')
 
 const app = express();
 
@@ -13,25 +14,54 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Product = require('./models/product');
 
-// db.execute('SELECT * FROM products ')
-//     .then(result => {
-//         // console.log(result)
-//     })
-//     .catch(() => {
-//         console.log(err)
-//     })
-
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user
+            console.log("//////////////////")
+            console.log(user)
+            next()
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    // console.log(result)
-    app.listen(3000)
-}).catch(err => console.log(err))
+// Associations
+
+Product.belongsTo(User, {
+    constrainsts: true,
+    onDelete: 'CASCADE'
+})
+
+User.hasMany(Product)
+
+
+sequelize
+    .sync()
+    // .sync({ force: true }) // only for production and development 
+    .then(result => {
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: "max", email: "test@test.com" })
+        }
+        return user
+    })
+    .then(user => {
+        // console.log(user)
+        app.listen(3000)
+    })
+    .catch(err => console.log(err))
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,15 +13,17 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    Product.create({
+    console.log(title)
+    req.user.createProduct({
         title: title,
         price: price,
         imageUrl: imageUrl,
         description: description
-    }).then(result => {
-        console.log('created product')
-        res.redirect('/')
-    }).catch(err => console.log(err))
+    })
+        .then(result => {
+            console.log('created product')
+            res.redirect('/')
+        }).catch(err => console.log(err))
     // const product = new Product(null, title, imageUrl, description, price);
     // product.save().then(() => {
     //     res.redirect('/');
@@ -35,7 +37,10 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findByPk(prodId).then(product => {
+    req.user.getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+        const product = products[0]
         if (!product) {
             return res.redirect('/');
         }
@@ -83,7 +88,10 @@ exports.postEditProduct = async (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll().then(products => {
+    
+    // Product.findAll()
+    req.user.getProducts()
+    .then(products => {
         res.render('admin/products', {
             prods: products,
             pageTitle: 'Admin Products',
